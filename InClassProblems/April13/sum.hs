e81442d4e970ac8f9b5e402d720cf9b238be3741
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-04-10 19:27:42 -0500

    Update sum.hs

--- InClassProblems/April13/sum.hs
+++ InClassProblems/April13/sum.hs
@@ -1,29 +1,33 @@
-
 {-
-sum
-1. define the type
+Using the five-step process construct the library function that calculates the sumof a list of numbers listing each step:
+
+•Step 1: Define the type
 sum :: [Int] -> Int
-2. enumerate the cases
+
+•Step 2: Enumerate the cases
 sum []
 sum [x]
 sum [x, y]
 sum (x : xs)
-3. define the simple cases
+
+•Step 3: Define the simple cases
 sum []       = 0
 sum [x]      = x
 sum [x, y]   = x + y
 sum (x : xs) = ...
-4. define other cases
+
+•Step 4: Define the other cases
 sum []       = 0
 sum [x]      = x
 sum [x, y]   = x + y
 sum (x : xs) = x + sum xs
-5. generalize and simplify
+
+•Step 5: Generalize and simplify
 sum []       = 0
 sum (x : xs) = x + sum xs
--}
+
+Final Function| library function that calculates the sumof a list of numbers listing each step: -}
 
 sum :: [Int] -> Int
 sum []       = 0
 sum (x : xs) = x + sum xs
--}
