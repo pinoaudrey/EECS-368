e50f33962a6b8c9e320a8ccdb89bb5c2436921ae
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-04-20 16:15:42 -0500

    Update putStr.hs

--- InClassProblems/April20/putStr.hs
+++ InClassProblems/April20/putStr.hs
@@ -1,2 +1,10 @@
+1.
 putStr' :: String -> IO ()
 putStr' xs = sequence_ [putChar x | x <- xs] -- build a list of putChar actions & evaluate with sequence_
+
+2. 
+charThree :: IO() -> (Char,Char)
+charThree = do x <- getChar
+               y <- getChar
+               z <- getChar
+               return (x,z)
