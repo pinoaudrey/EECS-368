c7f9b390e99dca6086ad5fb6b693af527b673cae
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-04-25 14:16:40 -0500

    Create sudoku.hs

---
+++ InClassProblems/April25/sudoku.hs
@@ -0,0 +1,15 @@
+1. Which row and column makes the Sudoku solution to the right invalid?
+row: 5th row, repeating 7's
+column: 3rd column, repeating 7's
+
+For each of the following what is the output (assume boxsize
+= 2)? Show each solution as lists with appropriate []’s.
+
+2. rows 
+[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+3. columns 
+[[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16]]
+4. boxs 
+[[1,2,5,6],[3,4,7,8],[9,10,13,14],[11,12,15,16]]
+5. collapse 
+[[[1,3],[4,1]],[[1,3],[4,2]],[[2,3],[4,4]],[[2,3],[4,2]] ]
