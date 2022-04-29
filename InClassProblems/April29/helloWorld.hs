e59315121069de4cc27fa8d5f140c4ea400e6c74
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-04-29 16:01:06 -0500

    Update helloWorld.hs

--- InClassProblems/April29/helloWorld.hs
+++ InClassProblems/April29/helloWorld.hs
@@ -5,8 +5,14 @@ main = putStrLn "Hello World!"
     
 2. Uses the do function, putStr, and putChar, with the two strings: “Hello” and “World!”.
 Recall: putStr :: String -> IO () writes a string without moving to a new line, and putChar :: Char -> IO () writes a single character to the screen.
-main = do   putStr "Hello "  
-            putStr "World"  
-            putChar "!"
+main = do   putStr "Hello"
+            putChar ' '
+            putStr "World!"  
 3. Uses the do, let, concat, and putStr keywords, with the two strings: “Hello” and “World!\n”.
 Recall: concatis a “pure” function that concatenates a list of strings.
+main = do
+let str1 = "Hello"
+let space = " "
+let str2 = "World!\n"
+let str3 = concat [str1, space, str2]
+putStr str3
