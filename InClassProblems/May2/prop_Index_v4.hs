2e85fbd39114f8478bfbb265242eef66eacccc09
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-05-02 13:15:26 -0500

    Update prop_Index_v4.hs

--- InClassProblems/May2/prop_Index_v4.hs
+++ InClassProblems/May2/prop_Index_v4.hs
@@ -2,15 +2,32 @@ prop_Index_v4 :: (NonEmptyList Integer) -> Property
 prop_Index_v4 (NonEmpty xs) =
 forAll (choose (0, length xs-1)) $ \n -> xs !! n == head (drop n xs)
 
-1. If the length of xs is 3, what values will QuickCheck choose from, to test prop_Index_v4.
-three :: [a] -> a
-three(x : xs) = if length xs == 2 then x else
-                three xs
-2. Generate a test list of 5 elements that QuickCheck might generate for Problem No. 1.
-testList :: [a] -> [a]
-testList [] = []
-testList [x] = [x]
-testList (x:y:xs0 = testList xs
+1. If the length of xs is 3, the values that QuickCheck choose from, to test prop_Index_v4 are from 0 to 2.
 
-3. Let xs = [1,2,3], show prop_Index_v4 is true for each of the values you determined in Problem No. 1. For example, if n = 0, show:
-xs !! 0 == head (drop 0 xs)
+2. 
+[0,1,2,0,1]
+
+3. 
+xs !! 0 == head (drop 0 xs):
+head( drop 0 xs)
+  [1,2,3]
+  =1
+xs !! 0
+  [1,2,3]
+  =1
+
+xs !! 1 == head (drop 1 xs):
+head( drop 1 xs)
+  [2,3]
+  =2
+ xs !! 1
+   [1,2,3]
+  =2
+  
+xs !! 2 == head (drop 2 xs):
+head( drop 2 xs)
+  [3]
+  =3
+ xs !! 2
+   [1,2,3]
+  =3
