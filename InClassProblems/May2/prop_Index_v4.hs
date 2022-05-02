c5edd62604fc728fb3baee647bb62a36ac6e63a7
pinoaudrey <91105614+pinoaudrey@users.noreply.github.com>
2022-05-01 21:01:53 -0500

    Update prop_Index_v4.hs

--- InClassProblems/May2/prop_Index_v4.hs
+++ InClassProblems/May2/prop_Index_v4.hs
@@ -3,8 +3,14 @@ prop_Index_v4 (NonEmpty xs) =
 forAll (choose (0, length xs-1)) $ \n -> xs !! n == head (drop n xs)
 
 1. If the length of xs is 3, what values will QuickCheck choose from, to test prop_Index_v4.
-
+three :: [a] -> a
+three(x : xs) = if length xs == 2 then x else
+                three xs
 2. Generate a test list of 5 elements that QuickCheck might generate for Problem No. 1.
+testList :: [a] -> [a]
+testList [] = []
+testList [x] = [x]
+testList (x:y:xs0 = testList xs
 
 3. Let xs = [1,2,3], show prop_Index_v4 is true for each of the values you determined in Problem No. 1. For example, if n = 0, show:
 xs !! 0 == head (drop 0 xs)
